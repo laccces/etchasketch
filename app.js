9cc879f52a86c0c2fa896f4ac89a8276b6c31a49
Dan <en07djc@icloud.com>
2022-03-15 15:45:44 +0000

    Initial code to work out how to draw the grid.

---
+++ app.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("container");
+
+// const cell = document.createElement('div');
+
+function drawGrid(gridRows, gridColumns) {
+    //let gridRows = Number(gridRows);
+    //let gridColumns = Number(gridColumns);
+
+    for (let i = 0; i <  gridRows; i++) {
+       // var newDiv = document.createElement('div');
+       return cell
+    }
+
+}
+
+drawGrid(16, 16)
+
+container.appendChild(cell);
