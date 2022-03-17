71c79cbb1e8c5b06a9bfa332c2d8c66d76284e93
Dan <en07djc@icloud.com>
2022-03-17 14:25:57 +0000

    Added reset button functionality.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 function drawBoard(size) {
     let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i=0; i<256; i++) {
+    let amount = size * size
+    for (let i =0 ; i < amount; i++) {
         let square = document.createElement('div');
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black'});
         board.insertAdjacentElement('beforeend', square);
     }
 }
@@ -12,5 +17,16 @@ function drawBoard(size) {
 drawBoard(16);
 
 function changeSize(input) {
-    drawBoard(input)
+    if(input >= 2 && input <= 100) {
+    drawBoard(input);
+    } else {
+        console.log('Error. Number must be between 2 and 100.');
+    }
+    
+}
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
 }
